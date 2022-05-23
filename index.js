a48b2b4bd7ba45f59adfa75fc041c55fc1b6f735
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-23 14:40:55 +0600

    create partcollection and sent to client side

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2k2a8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+      await client.connect();
+      const partCollection = client.db('phoenix_tech').collection('parts');
+
+      app.get('/part', async (req, res) => {
+        const query = {};
+        const cursor = partCollection.find(query);
+        const parts = await cursor.toArray();
+        res.send(parts);
+      });
+
+
+
+    }
+      finally {
+
+    }
+  }
+  
+  run().catch(console.dir);
+
 
 
 
