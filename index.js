bd6788b574afc610725678b6b57afd389561394d
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-24 00:05:06 +0600

    send user to client side

--- index.js
+++ index.js
@@ -69,10 +69,10 @@ async function run() {
         
       });
 
-    //   app.get('/user', async (req, res) => {
-    //     const users = await userCollection.find().toArray();
-    //     res.send(users);
-    //   });
+      app.get('/user', async (req, res) => {
+        const users = await userCollection.find().toArray();
+        res.send(users);
+      });
 
       app.put('/user/:email', async (req, res) => {
         const email = req.params.email;
