e423fcb4eb06d6a65c24479447240e67121033be
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-23 21:08:09 +0600

    add jwt

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2k2a8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
+  
+
 async function run() {
     try {
       await client.connect();
@@ -38,10 +56,13 @@ async function run() {
         const purchase = req.body;
         const result = await purchaseCollection.insertOne(purchase);
         res.send(result)
+    });
 
-       
+    app.get('/purchase', async (req, res) => {
+        const user = req.query.user;
+        const purchase = await purchaseCollection.find(user).toArray();
+         res.send(purchase);
       });
-
 
 
     }
